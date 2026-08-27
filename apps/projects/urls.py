de286ac2a705ca1b19c15a047f19451d32e991d0
R-M4NIK <133315583+rmanikk@users.noreply.github.com>
2026-08-27 16:18:09 +0545

    Project Page rework

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+
 from . import views
 
 
+app_name = "projects"
+
+
 urlpatterns = [
-    path("", views.project_list, name="projects"),
+    path("", views.project_list, name="project_list"),
 ]
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from .models import Project
+
 
 def project_list(request):
-    return render(request, "projects/index.html")
+    projects = Project.objects.all()
+
+    context = {
+        "projects": projects,
+    }
+
+    return render(
+        request,
+        "projects/index.html",
+        context,
+    )
