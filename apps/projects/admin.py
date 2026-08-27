dc141e61a6f906c4c628868622cb2e3becef2fb2
R-M4NIK <133315583+rmanikk@users.noreply.github.com>
2026-08-27 12:57:04 +0545

    admin works

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,3 +1,79 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "featured",
+        "order",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "featured",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "short_description",
+        "description",
+    )
+
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
+
+    ordering = (
+        "order",
+        "-created_at",
+    )
+
+    fieldsets = (
+        (
+            "Project Information",
+            {
+                "fields": (
+                    "title",
+                    "slug",
+                    "short_description",
+                    "description",
+                    "image",
+                )
+            },
+        ),
+        (
+            "Project Links",
+            {
+                "fields": (
+                    "github_url",
+                    "live_url",
+                )
+            },
+        ),
+        (
+            "Technologies",
+            {
+                "fields": (
+                    "technologies",
+                ),
+                "description": (
+                    'Enter technologies as JSON. Example: '
+                    '["Django", "PostgreSQL", "JavaScript"]'
+                ),
+            },
+        ),
+        (
+            "Homepage",
+            {
+                "fields": (
+                    "featured",
+                    "order",
+                )
+            },
+        ),
+    )
--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+
+    slug = models.SlugField(
+        max_length=220,
+        unique=True,
+        blank=True
+    )
+
+    short_description = models.CharField(
+        max_length=300
+    )
+
+    description = models.TextField()
+
+    image = models.ImageField(
+        upload_to="projects/"
+    )
+
+    github_url = models.URLField(
+        blank=True
+    )
+
+    live_url = models.URLField(
+        blank=True
+    )
+
+    technologies = models.JSONField(
+        default=list,
+        blank=True
+    )
+
+    featured = models.BooleanField(
+        default=False
+    )
+
+    order = models.PositiveIntegerField(
+        default=0
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    class Meta:
+        ordering = ["order", "-created_at"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
