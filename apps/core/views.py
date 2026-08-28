e04b284efdace86fc7f14d7e896b3948598398fe
R-M4NIK <133315583+rmanikk@users.noreply.github.com>
2026-08-28 08:10:46 +0545

    Home Page

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+from apps.projects.models import Project
+from apps.blog.models import BlogPost
+
 
 def home(request):
-    return render(request, "home/index.html")
+
+    featured_projects = Project.objects.filter(
+        featured=True
+    ).order_by("order", "-created_at")[:2]
+
+    featured_blogs = BlogPost.objects.filter(
+        featured=True,
+        published=True
+    ).order_by("-published_at", "-created_at")[:3]
+
+    return render(
+        request,
+        "home/index.html",
+        {
+            "featured_projects": featured_projects,
+            "featured_blogs": featured_blogs,
+        },
+    )
